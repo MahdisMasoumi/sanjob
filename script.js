865f0d434a1a5d65a958085a74cb61d2f08f31e1
Mahdis <mahdis.masoumi.sh@gmail.com>
2023-10-06 10:38:08 -0600

    changing footer for mobile

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document
   .getElementById("searchInput")
   .addEventListener("keyup", function (event) {
     if (event.key === "Enter") {
-      event.preventDefault(); // Prevents the form from actually submitting
-      document.getElementById("searchForm").submit(); // Submits the form
+      event.preventDefault();
+      document.getElementById("searchForm").submit();
     }
   });
